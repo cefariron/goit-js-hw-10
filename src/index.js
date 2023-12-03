d1023555fd5ec8eca5d1251ad1275fa8d480f0c9
cefariron <adidaswww17@gmail.com>
2023-12-03 11:51:04 +0200

    complete hw10 without slim select

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { TheCatAPI } from '@thatapicompany/thecatapi';
 import axios from 'axios';
 import SlimSelect from 'slim-select'
 
-// new SlimSelect({
-//   select: '#selectElement'
-// })
+// new SlimSelect({               //  Библиотека SlimSelect. Подключена, но если
+//   select: '#selectElement'     //  инициализировать ее, все ломается. Как 
+// })                             //  исправить незнаю. Сдаю без нее.
 
 new TheCatAPI(
   'live_s8nipvOVu9x7M1Oib237fV73mUqJ5mMbosZrl0Spq6nATwMKfjepogDKSlr2wzo8'
